79fcbe3ad9270cd96dae1585b2515d6d4e847895
demig00d <28487425+demig00d@users.noreply.github.com>
2023-05-24 01:02:31 +0300

    Fix to get actors without a photo

--- server/services/roles.js
+++ server/services/roles.js
@@ -6,16 +6,14 @@ module.exports = ({ strapi }) => ({
 
   async listRoles(id) {
     let { rows } = await strapi.db.connection.raw(
-      ` SELECT ar.id, a.id as actor_id, a.title as name, pr.role_title as role, f.url as src, pl.play_role_order as order
+       `SELECT ar.id, a.id as actor_id, a.title as name, pr.role_title as role, f.url as src, pl.play_role_order as order
         FROM play_roles_play_links pl
         JOIN play_roles pr ON pl.play_role_id=pr.id
         JOIN persons_play_roles_links ar ON ar.play_role_id=pl.play_role_id
         JOIN persons a ON a.id=ar.person_id
-        JOIN files_related_morphs r ON r.related_id = a.id
-        JOIN files f ON f.id = r.file_id
-        WHERE r.field='cover'
-        AND r.related_type = 'api::person.person'
-        AND pl.play_id = (select play_id from shows_play_links where show_id = ?);`, [id]
+        LEFT JOIN files_related_morphs r ON (r.related_id = a.id AND r.field='cover' AND r.related_type = 'api::person.person')
+        LEFT JOIN files f ON f.id = r.file_id
+        WHERE pl.play_id = (select play_id from shows_play_links where show_id = ? order by id desc limit 1);`, [id]
     ).catch(err => {
       strapi.log.warn(err)
     });
